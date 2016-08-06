9481786ba98655980fc285c246551089ced6ec54
johaen11 <johngnanakumar@gmail.com>
2016-08-06 14:35:57 -0400

    Initial Commit

--- COMP123-S2016-ASSIGNMENT7/Program.cs
+++ COMP123-S2016-ASSIGNMENT7/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/**
+ * Author: Johaen Gnanakumar
+ * Date: August 06, 2016
+ * StudentID: 300880309
+ * Description: a multi-form project that allows the user to select	a Movie	to view	from an	online	video streaming	service	called	“Movie	Bonanza”.
+ * Version: 0.0.1
+ */
+
 namespace COMP123_S2016_ASSIGNMENT7
 {
     static class Program
